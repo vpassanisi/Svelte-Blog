a392b983127daa34fe9f5953b9b4eeee7bb9043c
vpassanisi <vpassanisi@gmail.com>
2020-04-24 16:51:05 -0700

    rename

--- src/pages/RMS.svelte
+++ src/pages/RMS.svelte
@@ -1,7 +1,7 @@
 <script>
   import { media } from "../stores/watchMedia.js";
   import Player from "../components/Player.svelte";
-  import D3test from "../components/d3test.svelte";
+  import PlayerAnalyzer from "../components/PlayerAnalyzer.svelte";
   import P5test from "../components/P5Test.svelte";
 </script>
 
@@ -14,4 +14,4 @@
 <div
   class={`bg-img ${$media.lg ? 'bg-fixed' : ''} bg-cover bg-no-repeat bg-right-bottom w-full h-50vh`} />
 <div id="post" class="dark:text-white mx-auto py-24 w-90p max-w-screen-lg" />
-<D3test />
+<PlayerAnalyzer />
